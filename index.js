427dd4638104fce7451a72ee13245010731da2ba
Kunagorn Sirikupt <kunagorn.sirikupt@gmail.com>
2024-08-19 00:52:25 +0700

    Add CORS header to app

--- index.js
+++ index.js
@@ -1,92 +1,78 @@
-const express = require('express')
-const oracledb = require('oracledb');
+const express = require("express");
+const oracledb = require("oracledb");
 const app = express();
 const port = 3000;
-var password = 'sdtvend';
+var password = "sdtvend";
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+// Initialize Oracle Client for Thick mode
+try {
+  oracledb.initOracleClient({ libDir: "C:\\oracle\\instantclient_23_4" }); // Adjust the path as necessary
+} catch (err) {
+  console.error("Failed to initialize Oracle Client:", err);
+  process.exit(1);
+}
 
 async function selectAllEmployees(req, res) {
   try {
-    connection = await oracledb.getConnection({
+    const connection = await oracledb.getConnection({
       user: "sdtvend",
       password: password,
-      connectString: "192.168.1.204:1521/sdtdb"
+      connectString: "192.168.1.204:1521/sdtdb",
     });
 
-    console.log('connected to database');
+    console.log("GET - All employee data");
     // run query to get all employees
     result = await connection.execute(`SELECT * FROM vdb_det`);
-    console.log(result)
+    return res.send(result);
   } catch (err) {
     //send error message
     return res.send(err.message);
-  } finally {
-    if (connection) {
-      try {
-        // Always close connections
-        await connection.close();
-        console.log('close connection success');
-      } catch (err) {
-        console.error(err.message);
-      }
-    }
-    if (result.rows.length == 0) {
-      //query return zero employees
-      return res.send('query send no rows');
-    } else {
-      //send all employees
-      return res.send(result.rows);
-    }
-
   }
 }
 
 //get /employess
-app.get('/employees', function (req, res) {
+app.get("/employees", function (req, res) {
   selectAllEmployees(req, res);
-})
+});
 
 async function selectEmployeesById(req, res, id) {
   try {
     connection = await oracledb.getConnection({
       user: "hr",
       password: password,
-      connectString: "localhost:1521/xepdb1"
+      connectString: "localhost:1521/xepdb1",
     });
     // run query to get employee with employee_id
-    result = await connection.execute(`SELECT * FROM employees where employee_id=:id`, [id]);
-
+    result = await connection.execute(
+      `SELECT * FROM employees where employee_id=:id`,
+      [id]
+    );
   } catch (err) {
     //send error message
     return res.send(err.message);
-  } finally {
-    if (connection) {
-      try {
-        // Always close connections
-        await connection.close(); 
-      } catch (err) {
-        return console.error(err.message);
-      }
-    }
-    if (result.rows.length == 0) {
-      //query return zero employees
-      return res.send('query send no rows');
-    } else {
-      //send all employees
-      return res.send(result.rows);
-    }
   }
 }
 
 //get /employee?id=<id employee>
-app.get('/employee', function (req, res) {
+app.get("/employee", function (req, res) {
   //get query param ?id
   let id = req.query.id;
   // id param if it is number
   if (isNaN(id)) {
-    res.send('Query param id is not number')
-    return
+    res.send("Query param id is not number");
+    return;
   }
   selectEmployeesById(req, res, id);
-})
+});
 
-app.listen(port, () => console.log("nodeOracleRestApi app listening on port %s!", port))
+app.listen(port, () =>
+  console.log("nodeOracleRestApi app listening on port %s!", port)
+);
